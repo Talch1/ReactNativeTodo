9c01b4562647c0c47f0b03ab0b54954941817064
Anatoly <geylert@gmail.com>
2020-07-17 14:52:02 +0300

    add context

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,29 +1,35 @@
-import React, { useReducer } from "react";
-import { TodoContext } from "./todoContext";
-import { todoReducer } from "./todoReducer";
-import { ADD_TODO, DELETE_TODO, UPDATE_TODO } from "../types";
+import React, { useReducer, useContext } from 'react'
+import { TodoContext } from './todoContext'
+import { todoReducer } from './todoReducer'
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
+import { ScreenContext } from '../screen/screenContext'
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [{ id: "1", title: "Learn React Native" }],
-  };
+    todos: [{ id: '1', title: 'Выучить React Native' }]
+  }
+  const { changeScreen } = useContext(ScreenContext)
+  const [state, dispatch] = useReducer(todoReducer, initialState)
 
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const addTodo = title => dispatch({ type: ADD_TODO, title })
 
-  const addTodo = (title) => dispatch({ type: ADD_TODO, title });
-  const removeTodo = (id) => dispatch({ type: DELETE_TODO, id });
-  const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+  const removeTodo = id => {
+    changeScreen(null)
+    dispatch({ type: REMOVE_TODO, id })
+  }
+
+  const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
 
   return (
     <TodoContext.Provider
       value={{
         todos: state.todos,
         addTodo,
-        updateTodo,
         removeTodo,
+        updateTodo
       }}
     >
       {children}
     </TodoContext.Provider>
-  );
-};
+  )
+}

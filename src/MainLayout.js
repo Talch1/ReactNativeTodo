e66b5837c77f08419b0bfdea09aa2b56a85cf6a4
Anatoly <geylert@gmail.com>
2020-07-17 18:55:31 +0300

    refactor

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,15 +1,14 @@
-import React, { useState, useContext } from 'react'
+import React, {  useContext } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Navbar } from './components/Navbar'
 import { THEME } from './theme'
 import { MainScreen } from './screens/MainScreen'
 import { TodoScreen } from './screens/TodoScreen'
-import { TodoContext } from './context/todo/todoContext'
+
 import { ScreenContext } from './context/screen/screenContext'
 
 export const MainLayout = () => {
-  const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext)
-  const { todoId, changeScreen } = useContext(ScreenContext)
+  const { todoId} = useContext(ScreenContext)
 
   // const removeTodo = id => {
   //   const todo = todos.find(t => t.id === id)
@@ -34,31 +33,14 @@ export const MainLayout = () => {
   //   )
   // }
 
-  let content = (
-    <MainScreen
-      todos={todos}
-      addTodo={addTodo}
-      removeTodo={removeTodo}
-      openTodo={changeScreen}
-    />
-  )
 
-  if (todoId) {
-    const selectedTodo = todos.find(todo => todo.id === todoId)
-    content = (
-      <TodoScreen
-        onRemove={removeTodo}
-        goBack={() => changeScreen(null)}
-        todo={selectedTodo}
-        onSave={updateTodo}
-      />
-    )
-  }
+
+
 
   return (
     <View>
       <Navbar title='Todo App!' />
-      <View style={styles.container}>{content}</View>
+      <View style={styles.container}>{todoId? <TodoScreen/>:<MainScreen/>}</View>
     </View>
   )
 }
--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,17 +1,23 @@
-import React, { useState } from 'react'
-import { StyleSheet, View, Button, Dimensions } from 'react-native'
+import React, { useState ,useContext} from 'react'
+import { StyleSheet, View,Dimensions } from 'react-native'
 import { FontAwesome, AntDesign } from '@expo/vector-icons'
 import { THEME } from '../theme'
 import { AppCard } from '../components/ui/AppCard'
 import { EditModal } from '../components/EditModal'
 import { AppTextBold } from '../components/ui/AppTextBold'
 import { AppButton } from '../components/ui/AppButton'
+import {TodoContext} from '../context/todo/todoContext';
+import { ScreenContext } from '../context/screen/screenContext'
 
-export const TodoScreen = ({ goBack, todo, onRemove, onSave }) => {
+
+export const TodoScreen = () => {
   const [modal, setModal] = useState(false)
 
+  const {todos,removeTodo,updateTodo} = useContext(TodoContext)
+  const {changeScreen,todoId}  = useContext(ScreenContext)
+  const todo = todos.find(t=>t.id === todoId)
   const saveHandler = title => {
-    onSave(todo.id, title)
+    updateTodo(todo.id, title)
     setModal(false)
   }
 
@@ -33,14 +39,14 @@ export const TodoScreen = ({ goBack, todo, onRemove, onSave }) => {
 
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <AppButton onPress={goBack} color={THEME.GREY_COLOR}>
+          <AppButton onPress={()=>changeScreen(null)} color={THEME.GREY_COLOR}>
             <AntDesign name='back' size={20} color='#fff' />
           </AppButton>
         </View>
         <View style={styles.button}>
           <AppButton
             color={THEME.DANGER_COLOR}
-            onPress={() => onRemove(todo.id)}
+            onPress={() => removeTodo(todo.id)}
           >
             <FontAwesome name='remove' size={20} color='#fff' />
           </AppButton>
